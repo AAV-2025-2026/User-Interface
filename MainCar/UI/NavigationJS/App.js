851759bc10e0f9d1e4f6f7341ea6dcb5e2dd5025
Samson Ha <whiteram81@gmail.com>
2025-10-27 10:50:10 -0400

    Add UI, Initial

---
+++ MainCar/UI/NavigationJS/App.js
@@ -0,0 +1,42 @@
+// App.js
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+import TopStatusBar from './components/TopStatusBar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TopStatusBar />
+
+      <View style={styles.content}>
+        {/* Sidebar */}
+        <View style={styles.sidecontainer}>
+          <SearchBar style={{ marginBottom: 10 }} />
+          <SearchResults />
+        </View>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+  },
+  content: {
+    flex: 1,
+    flexDirection: 'row', 
+  },
+  sidecontainer: {
+    width: '28%', 
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: 10,
+  },
+});
