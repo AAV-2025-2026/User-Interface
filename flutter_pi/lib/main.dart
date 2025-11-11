411885305ac25ebbd94172a6d7d2d9a6ee72d1b5
shabesa <k.a.shabesa@gmail.com>
2025-11-11 22:54:05 +0530

    fixed async await

--- flutter_pi/lib/main.dart
+++ flutter_pi/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Initialize window manager
   await windowManager.ensureInitialized();
