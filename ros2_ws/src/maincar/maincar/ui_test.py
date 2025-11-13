88c3173d39562fdb3b906765ab6f9d61b48cb537
Lexyvil <Alex_Laframboise@hotmail.com>
2025-11-13 01:43:09 -0500

    added gps mock data for ros2 flask

--- ros2_ws/src/maincar/maincar/ui_test.py
+++ ros2_ws/src/maincar/maincar/ui_test.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """
-This node constantly sends a Float32 value every second as mock data.
+This node constantly sends mock data to the UI for testing purposes:
+
+Float32 speed
+GPS data
+
+ value every second as mock data.
 It is for the UI (Flask server) to fetch and display for testing purposes.
 Pretend the mocked data is car speed.
 """
@@ -10,15 +15,18 @@ from rclpy.node import Node
 import random
 
 from std_msgs.msg import Float32
+from sensor_msgs.msg import NavSatFix
+
 # from geometry_msgs.msg import Twist
 
 
-class MyNode(Node):
+class MockNode(Node):
     
     def __init__(self):
-        super().__init__("mock_car_speed_node")
-        
-        # we want this node to publish to topic called /mock_speed
+        super().__init__("mock_sensors_node")
+        self.coord_set = 0
+
+        # we want this node to publish to topics reserved for mock data
         # self.create_publisher(type_of_msg, topic_name, queue_size)
         
         # creates publisher
@@ -26,31 +34,62 @@ class MyNode(Node):
                                                   "/mock_speed",    # topic name
                                                   10)               # QoS profile
         
+        self.cmd_gps_pub_ = self.create_publisher(NavSatFix,        # msg type
+                                            "/mock_gps",            # topic name
+                                            10)                     # QoS profile
+        
 
-        self.get_logger().info("This node now sends a mock Float32 value each second at topic /mock_speed")
+        
 
         # set call function every x seconds
-        self.timer_ = self.create_timer(1,                                   # amount of seconds to wait 
-                                        self.send_mock_velocity_command)     # callback-function to call
+        self.timer_vel = self.create_timer(1,                                   # amount of seconds to wait 
+                                        self.send_mock_data)     # callback-function to call
+        
+
+        self.get_logger().info("This node now sends a mock speed (Float32) to topic /mock_speed, and mock gps data to topic /mock_gps")
 
 
+    def send_mock_data(self):
 
-    def send_mock_velocity_command(self):
-
+        # === Mock Data for Speed ===
+          # creates mock velocity between 4.0 and 6.0
         msg = Float32()
 
-        # creates mock velocity between 4.0 and 6.0
+        
         msg.data = random.uniform(4.0, 6.0)
-        self.get_logger().info("Sending Mock...")
+        self.get_logger().info("Sending Mock Speed...")
+        self.cmd_vel_pub_.publish(msg)  # publishes speed message
 
+        # === Mock Data for GPS to publish ===
 
-        self.cmd_vel_pub_.publish(msg)  # publishes message
+        # canada parliament: 45.42410246146506, -75.69894697494593   (0.00001 decimal places for degrees for 1.11 m)
+        # carleton university parking: 45.38382089559656, -75.69658483653251
+        msgGPS = NavSatFix()
+
+        if (self.coord_set <= 2):
+
+            msgGPS.latitude = 45.42410
+            msgGPS.longitude = -75.69895
+            msgGPS.altitude = 1.0
+
+        elif (self.coord_set <= 5):
+
+            msgGPS.latitude = 45.38382
+            msgGPS.longitude = -75.69658
+            msgGPS.altitude = 2.0
+
+        #latitude = msg.latidude
+
+        self.cmd_gps_pub_.publish(msgGPS) # publishes to /mock_gps
+        self.coord_set += 1
+        if (self.coord_set > 5): self.coord_set = 0
+        self.get_logger().info("Sending Mock GPS... (coord set: +"+str(self.coord_set)+")")
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    node = MyNode()
+    node = MockNode()
     rclpy.spin(node) # keeps node alive until CTRL+C command is used
 
     rclpy.shutdown()
